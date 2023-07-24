06c3831f99d9deb8357b55a098fd8548d5d95281
Maxx1221 <somanathcharan9@gmail.com>
2023-07-24 15:54:58 +0000

    rova :Droidx Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/cherish_rova.mk
+    $(LOCAL_DIR)/droidx_rova.mk
 
 COMMON_LUNCH_CHOICES := \
-    cherish_rova-user \
-    cherish_rova-userdebug \
-    cherish_rova-eng
+    droidx_rova-user \
+    droidx_rova-userdebug \
+    droidx_rova-eng
--- droidx_rova.mk
+++ droidx_rova.mk
@@ -9,22 +9,22 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 
-# Inherit some common octavi stuff.
-$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
+# Inherit some common droidx stuff.
+$(call inherit-product, vendor/droidx/config/common_full_phone.mk)
 
 # Inherit from rova device
 $(call inherit-product, device/xiaomi/rova/device.mk)
 
 # Flags
+DROIDX_BUILD_TYPE := OFFICIAL
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_CALL_RECORDING := true
-CHERISH_VANILLA := true
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.cherish.maintainer= @maxx459
+    ro.droidx.maintainer= @maxx459
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := rova
-PRODUCT_NAME := cherish_rova
+PRODUCT_NAME := droidx_rova
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 4A / 5A
